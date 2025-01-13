e1ee6c85988ca5a5a0e9ee3e6cf9acb9a41cb510
zollien <dov19003@byui.edu>
2025-01-13 18:43:26 +0000

    w01 02 editied

--- scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-
-// Display the last modified date
-const lastModified = document.lastModified;
-document.getElementById("LastModified").textContent = lastModified;
-
-// Display the current year
-const currentYear = new Date().getFullYear();
-document.getElementById("Year").textContent = currentYear;
